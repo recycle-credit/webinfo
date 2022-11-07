ca7acbcaf1fa83893f4ced91a5e9cecaa10eb00b
Elemsimon <elemifesinachisimon@gmail.com>
2022-11-07 17:00:39 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AboutUs from "./pages/AboutPage";
+import Contact from "./pages/ContactUsPage/Contact";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route  path="/" exact element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+  </Router>
+    )
 }
 
 export default App;
